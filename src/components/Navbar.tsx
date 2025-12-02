216dded72ef5dc5f193701de3142ab0d981d1104
Jordan Fleming <insanitylegend35@gmail.com>
2025-12-02 10:42:27 -0500

    Improve EmployerWelcome popup mobile responsiveness and add button to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,17 @@
 
+
 import './Navbar.css';
+import NavbarEmployerButton from './NavbarEmployerButton';
+
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="navbar-links">
         {/* ...existing links, now removed... */}
+        <li>
+          <NavbarEmployerButton />
+        </li>
       </ul>
     </nav>
   );
